31d9d90715b961c50e6bad83f4f943d87c95bb81
Achmad Effendi <32183589+AchmadEffendi@users.noreply.github.com>
2017-09-22 10:54:28 +0700

    Add files via upload

---
+++ tugas1-04317031.py
@@ -0,0 +1,9 @@
+nama = "Achmad Effendi"
+nim = "04317031"
+umur = 19
+print ("Namaku:" + nama)
+print ("NIM:" + nim)
+print ("Umur:")
+print (umur)
+print ("Umur 5 tahun ke depan:")
+print (umur +5)
